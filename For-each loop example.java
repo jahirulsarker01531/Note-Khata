f62bb474e37b78512d39bfea3c8fc6e9fa0b8cba
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-09-28 10:21:45 +0600

    Create For-each loop example.java

---
+++ For-each loop example.java
@@ -0,0 +1,10 @@
+public class Simple{
+	public static void main(String[] args){
+		String[] x = {"Java", "C", "C++", "Python", "JavaScript"};
+		System.out.println("Printing the content of the array names: ");
+
+		for(String name:x){
+			System.out.println(name);
+		}
+	}
+}
